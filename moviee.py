5bb34d41c28e4c01442cb04809d5c7af13078116
adityakhanijow <aditya.khanijow@gmail.com>
2018-06-14 22:19:51 +0530

    Finding similar movies to a given movie

--- moviee.py
+++ moviee.py
@@ -5,3 +5,13 @@ m_cols=['movie_id','title']
 movies=pd.read_csv('u.item.csv',sep='|',names=m_cols,usecols=range(2))
 ratings=pd.merge(movies,ratings)
 ratings.head()
+movieRatings=ratings.pivot_table(index=['user_id'],columns=['title'],values='rating')
+movieRatings.head()
+youngF=movieRatings['Young Frankenstein (1974)']
+youngF.head()
+similarMovies=movieRatings.corrwith(youngF)
+similarMovies=similarMovies.dropna()
+df=pd.DataFrame(similarMovies)
+df.head(10)
+similarMovies.sort_values(ascending=False) # redency coz many values are 1 resolved furthur
+
